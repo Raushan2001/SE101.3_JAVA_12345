d58a26be9de9cafc5d441a90bb22b33f2890fea4
raushan_rashid <72301703+Raushan2001@users.noreply.github.com>
2023-06-12 12:52:17 +0530

    Add files via upload
    Added the Q1 of the Practical_05

---
+++ Practical_05/Q1/InterfaceImplemented.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication26;
+
+/**
+ *
+ * @author NSBM
+ */
+public class InterfaceImplemented implements MyFirstInterface {
+    
+    @Override
+    public void display(){
+        //x=9; 
+        
+        System.out.println(x);
+           
+}
+}
